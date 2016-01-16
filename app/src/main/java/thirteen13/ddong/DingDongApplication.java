8d161b80e70d89446641193deab4c0467134984f
Jake Sebastian-Jones <trideceth12@msn.com>
2016-01-17 01:34:49 +1100

    Added extened Application Class

---
+++ app/src/main/java/thirteen13/ddong/DingDongApplication.java
@@ -0,0 +1,12 @@
+package thirteen13.ddong;
+
+import android.app.Application;
+
+public class DingDongApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
