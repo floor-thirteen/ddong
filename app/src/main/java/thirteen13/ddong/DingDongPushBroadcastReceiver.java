e460cbf8f86db05c2b7ce0dd0b887699da791fc1
Jake Sebastian-Jones <trideceth12@msn.com>
2016-01-17 03:16:18 +1100

    Push notification now rings the ding dong

--- app/src/main/java/thirteen13/ddong/DingDongPushBroadcastReceiver.java
+++ app/src/main/java/thirteen13/ddong/DingDongPushBroadcastReceiver.java
@@ -3,21 +3,34 @@ package thirteen13.ddong;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.media.MediaPlayer;
 
 import com.parse.ParsePushBroadcastReceiver;
 
-/**
- * Created by jseba on 17/01/16.
- */
 public class DingDongPushBroadcastReceiver extends ParsePushBroadcastReceiver {
 
     @Override
     public void onPushReceive(Context context, Intent receivedIntent) {
         Log.d("FOO", "onPushReceive");
-        Intent intent = new Intent();
-        intent.setClassName("thirteen13.ddong", "thirteen13.ddong.DingDong");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+        MediaPlayer mPlayer = MediaPlayer.create(context, R.raw.doorbell);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            int n = 0;
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                n++;
+                if (n < 3) {
+                    mp.start();
+                }
+            }
+        });
+        
+        mPlayer.start();
+
+        super.onPushReceive(context, receivedIntent);
+
     }
 
 }
