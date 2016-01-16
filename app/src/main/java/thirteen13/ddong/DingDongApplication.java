1580aeb10fb7cd720c3bac0c8c1c00428f983157
Jake Sebastian-Jones <trideceth12@msn.com>
2016-01-17 01:42:40 +1100

    Added Parse configuration from QuickStart

--- app/src/main/java/thirteen13/ddong/DingDongApplication.java
+++ app/src/main/java/thirteen13/ddong/DingDongApplication.java
@@ -1,12 +1,20 @@
 package thirteen13.ddong;
 
 import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 public class DingDongApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Parse.initialize(this,
+                "eT8HUCePhitJSxgQSQOjRFko38GIbNUJu9AxNGeE",
+                "h05Qv9mz2A0oZK3Jq7cDuwaDdRIXtbbUqVcbalj7");
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
 }
