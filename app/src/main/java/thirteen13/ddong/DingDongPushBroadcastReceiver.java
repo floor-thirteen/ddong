9475445ba0545878eed6cba3154efb1364ba0720
Jake Sebastian-Jones <trideceth12@msn.com>
2016-01-26 03:58:39 +1100

    Respect ring mode (silent/vibrate)

--- app/src/main/java/thirteen13/ddong/DingDongPushBroadcastReceiver.java
+++ app/src/main/java/thirteen13/ddong/DingDongPushBroadcastReceiver.java
@@ -2,6 +2,7 @@ package thirteen13.ddong;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.util.Log;
 import android.media.MediaPlayer;
 
@@ -13,10 +14,18 @@ public class DingDongPushBroadcastReceiver extends ParsePushBroadcastReceiver {
     public void onPushReceive(Context context, Intent receivedIntent) {
         Log.d("FOO", "onPushReceive");
 
-        DingDongMediaPlayer mp = new DingDongMediaPlayer(context, R.raw.doorbell);
-        ThreadedDingger d = new ThreadedDingger(mp);
-        Thread dings = new Thread(d);
-        dings.start();
+        AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        switch (am.getRingerMode()) {
+            case AudioManager.RINGER_MODE_SILENT:
+            case AudioManager.RINGER_MODE_VIBRATE:
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                DingDongMediaPlayer mp = new DingDongMediaPlayer(context, R.raw.doorbell);
+                ThreadedDingger d = new ThreadedDingger(mp);
+                Thread dings = new Thread(d);
+                dings.start();
+        }
 
         super.onPushReceive(context, receivedIntent);
 
